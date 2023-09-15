cf685f934eb22357679920f917e3cad1e3342364
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-09-15 17:04:05 +0300

    created structure
    Created pages, routes, loading and error

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+const Footer = () => {
+  return (
+  <footer className="container">
+ Created by &copy;Olena
+  </footer>
+)}
+
+export {Footer}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 
-
 export default function Home() {
   return (
   <h1>Welcome to next Js</h1>
