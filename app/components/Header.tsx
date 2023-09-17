b3cee91723819a4474bd09873a4c2fec2ea51052
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-09-17 13:12:04 +0300

    Navigation
    Added component Navigation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,17 @@
-import Link from "next/link"
+import { Navigation } from "./Navigation";
 
-const Header= () => {
-    return (
-<header >
-    <Link href="/">Home</Link>
-    <Link href="/blog">Blog</Link>
-    <Link href="/about">About</Link>
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
+
+const Header = () => {
+  return (
+    <header>
+      <Navigation navLinks={navItems} />
     </header>
-  )}
-  
-  export {Header}
+  );
+};
+
+export { Header };
