d49587f4b2508f7151619386f414b0b101e63b35
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-09-18 22:43:06 +0300

    some changes
    try cookies, headers, redirect

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server"
+import { headers, cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    const id = params.id
+
+    const headerList = headers()
+
+    const type = headerList.get('Content-Type')
+
+    const cookiesList = cookies()
+
+    const cook = cookiesList.get("Cookie_1")?.value
+
+    //logic delete post
+    // redirect('/blog')
+    return NextResponse.json({ id, type, cook })
+}
